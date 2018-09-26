49e277b9ec23d9d9cbe5e9cd561844dcdf1753df
Aphaphon <pui.aphaphon@gmail.com>
2018-09-26 09:47:10 +0700

    mvc api

---
+++ demoapi/Controllers/ProductController.1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using demoapi.Models;
+
+namespace demoapi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class ProductController : Controller
+    {
+        [HttpGet]        
+        public string AllProduct()
+        {
+            return "Hello";
+        }
+        [HttpGet("{name}")]        
+        public string Show(string name)
+        {
+            return "Hello : "+ name;
+        }
+        [HttpPost]        
+        public string CreateProduct ()
+        {
+            return "a";
+        }
+        [HttpPut]        
+        public string UpdateProduct()
+        {
+            return "b";
+        }
+        
+    }
+}
---
+++ demoapi/Models/Booklist.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace demoapi.Models
+{
+    public class Booklist
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public int price { get; set; }
+       
+    }
+}
